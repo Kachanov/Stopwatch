eea4d04f0e213a20f8de9171289cdf1cd0f0ea5e
Unknown <mr.kachanov@gmail.com>
2018-01-02 13:28:18 +0200

    add localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        host: "localhost",
+        port: 8080
     }
 };
