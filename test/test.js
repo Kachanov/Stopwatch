8e8daffc3328be544af1c17d4c8d9292c9b1613b
Unknown <mr.kachanov@gmail.com>
2018-01-03 13:57:59 +0200

    added passed test

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ import {minutes, seconds, minutesArrow, secondsArrow} from "../src/main";*/
 describe("array", function () {
     describe("#indexOf()", function () {
         it("should return -1 when the value is not present", function () {
-            assert.equal([1,2,3].indexOf(4), 10);
+            assert.equal([1,2,3].indexOf(4), -1);
         });
     });
 });
