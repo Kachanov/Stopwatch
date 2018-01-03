d4ec50a9db65d55871138aa470756bde9a459aa6
Unknown <mr.kachanov@gmail.com>
2018-01-03 13:52:01 +0200

    added fail test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 var assert = require("assert");
+/*
+import {Stopwatch} from "../src/stopwatch";
+import {startTime, currentTime, timer, timerID, pauseTime, stopwatch} from "../src/stopwatch";
+import {minutes, seconds, minutesArrow, secondsArrow} from "../src/main";*/
+
 describe("array", function () {
     describe("#indexOf()", function () {
         it("should return -1 when the value is not present", function () {
-            assert.equal([1,2,3].indexOf(4), -1);
+            assert.equal([1,2,3].indexOf(4), 10);
         });
     });
 });
