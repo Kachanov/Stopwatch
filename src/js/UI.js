6962f1faff5d8b186570bf1e8355ab775ac89668
Unknown <mr.kachanov@gmail.com>
2018-01-05 10:31:26 +0200

    add images into wepback

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,31 +1,15 @@
 import "../styles/styles.css";
-import clock_face from "../images/clockface.png";
-import seconds_arrow from "../images/seconds-arrow.png";
-import minutes_arrow from "../images/minutes-arrow.png";
 
-export {startPauseButton,stopButton,minutes, seconds, lapButton, secondsArrowPlace, minutesArrowPlace, lapsCart, clockFacePlace, secondsArrow, minutesArrow};
-export {clockFace};
+export {startPauseButton,stopButton,minutes, seconds, lapButton, lapsCart, secondsArrow, minutesArrow};
+
 
 let startPauseButton = document.getElementById("start-pause-button");
 let stopButton = document.getElementById("stop-button");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 let lapButton = document.getElementById("lap-button");
-let secondsArrowPlace = document.getElementById("seconds-arrow");
-let minutesArrowPlace = document.getElementById("minutes-arrow");
+let secondsArrow = document.getElementById("seconds-arrow");
+let minutesArrow = document.getElementById("minutes-arrow");
 let lapsCart = document.getElementById("laps");
-let clockFacePlace = document.getElementById("clock_face");
-
-
-let clockFace = new Image();
-clockFace.src = clock_face;
-clockFacePlace.appendChild(clockFace);
-
-let secondsArrow = new Image();
-secondsArrow.src = seconds_arrow;
-secondsArrowPlace.appendChild(secondsArrow);
-
-let minutesArrow = new Image();
-minutesArrow.src = minutes_arrow;
-minutesArrowPlace.appendChild(minutesArrow);
+let clockFace = document.getElementById("clock_face");
 
